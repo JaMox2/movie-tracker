218aae9bd83f066efa50b8ba04f919e64dbae8b5
JaMox2 <jeffreyvillamilwork@gmail.com>
2022-07-31 19:22:28 +0800

    added some backend

--- public/assets/JS.js
+++ public/assets/JS.js
@@ -1,3 +1,9 @@
+let showMenuButton = document.querySelector('.showMenuBtn')
+let menuUi = document.querySelector('.displayMenu')
+let isMenuShowing = false
+
+
+
 var swiper = new Swiper(".mySwiper", {
   slidesPerView: 6,
   spaceBetween: 10,
@@ -22,4 +28,24 @@ var swiper = new Swiper(".headerSwiper", {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
+})
+
+let readMoreButton = document.querySelectorAll('.readMore')
+readMoreButton.forEach((x,i)=>{
+  x.addEventListener('click', async(e)=>{
+    console.log('click', i+1)
+    let response = await fetch('/getInfo', {method: 'GET', headers: {'Content-type': 'application/json; charset=UTF-8'}});
+    let dataInJson = await response.json()
+    menuUi.style.display = 'grid'
+  })
+})
+
+
+showMenuButton.addEventListener('click', ()=>{
+  if(isMenuShowing == false){
+    // menuUi.style.
+    menuUi.style.display = 'none'
+  }else{
+    menuUi.style.display = 'grid'
+  }
 })
--- server.js
+++ server.js
@@ -5,10 +5,60 @@ const app = express()
 app.use('/public', express.static('public'))
 app.set('view engine', 'ejs')
 
-app.get('/', (req, res)=>{
-  res.render('index.ejs')
-})
+const API_KEY = 'bffafdd3'
+const GetTopRowMoviesURL = `https://www.omdbapi.com/?apikey=${API_KEY}&s=spider&type=movie`
+const GetShowsUrl = `https://www.omdbapi.com/?apikey=${API_KEY}&s=show&type=series`
 
+
+app.get('/', async (req, res)=>{
+  try{
+    let responses = await fetch(GetTopRowMoviesURL)
+    let dataInJson = await responses.json()
+    let dataWithMore = await Promise.all(dataInJson.Search.map(async(x,i)=>{
+      let moreInfo = await fetch(`http://www.omdbapi.com/?apikey=${API_KEY}&i=${x.imdbID}`)
+      let moreInfoInJson = await moreInfo.json()
+      return x = {
+        Title: x.Title,
+        Year: x.Year,
+        imdbID: x.imdbID,
+        Type: x.Type,
+        Poster: x.Poster,
+        Rank: i+1,
+        MoreInfo: moreInfoInJson
+      }
+    }))
+    let shows = await fetch(GetShowsUrl)
+    let showsInJson = await shows.json()
+    let showsWithMore = await Promise.all(
+      showsInJson.Search.map(async(x,i)=>{
+        let moreInfo = await fetch(`http://www.omdbapi.com/?apikey=${API_KEY}&i=${x.imdbID}`)
+        let moreInfoInJson = await moreInfo.json()
+        return x = {
+          Title: x.Title,
+          Year: x.Year,
+          imdbID: x.imdbID,
+          Type: x.Type,
+          Poster: x.Poster,
+          Rank: i+1,
+          MoreInfo: moreInfoInJson
+        }
+      })
+    )
+
+
+    res.render('index.ejs', {
+      movies: dataWithMore,
+      shows: showsWithMore
+    })
+  }catch(err){
+    console.log(err)
+  }
+  // res.render('index.ejs', {movies: placeHolderItems})
+})
+app.get('/getInfo', (req, res)=>{
+  console.log('getting into server')
+  res.json({msg:'hello'})
+})
 
 
 

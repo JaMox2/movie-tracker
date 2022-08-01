d3627000a3959277196f38324a43f9eb1a6d22f1
JaMox2 <jeffreyvillamilwork@gmail.com>
2022-08-01 17:31:16 +0800

    done

--- public/assets/JS.js
+++ public/assets/JS.js
@@ -1,16 +1,13 @@
 let showMenuButton = document.querySelector('.showMenuBtn')
 let menuUi = document.querySelector('.displayMenu')
-let isMenuShowing = false
+let mainBody = document.querySelector('body')
 
 
 
 var swiper = new Swiper(".mySwiper", {
   slidesPerView: 6,
   spaceBetween: 10,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
+
 })
 
 var swiper = new Swiper(".headerSwiper", {
@@ -30,22 +27,116 @@ var swiper = new Swiper(".headerSwiper", {
   },
 })
 
+function loadSearchedItem(items){
+  let showSearchedItems = document.querySelector('.showSearchedItems')
+  showSearchedItems.innerHTML = ''
+  items.forEach((x,i)=>{
+    let ssItemsCard = document.createElement('div')
+    // let img = document.createElement('img')
+    let ssItemsCardText = document.createElement('div')
+    let h6 = document.createElement('h6')
+    let span = document.createElement('span')
+
+    ssItemsCard.classList.add('ssItemsCard') // CARD
+    ssItemsCard.classList.add('readMore')
+    ssItemsCard.classList.add(`${x.imdbID}`)
+    ssItemsCard.addEventListener('click', async (e)=>{
+      console.log('clicked', i+1)
+      let response = await fetch('/getInfo', {method: 'POST', body: JSON.stringify({
+        imdbID: e.currentTarget.classList[e.currentTarget.classList.length - 1],
+      }), headers: {'Content-type': 'application/json; charset=UTF-8'}});
+      let dataInJson = await response.json()
+      displayCurrentMovie(dataInJson.itemInfo, true)
+      menuUi.style.display = 'grid'
+      mainBody.style.overflow = 'hidden'
+    })
+      if(x.Poster != "N/A"){
+        let img = document.createElement('img')
+        img.src = `${x.Poster}`
+        img.alt = `${x.Title}`
+        ssItemsCard.appendChild(img)
+      }
+      // 
+      ssItemsCardText.classList.add('ssItemsCardText')
+        h6.innerText = `${x.Title}`
+        ssItemsCardText.appendChild(h6)
+        span.innerText = `${x.Year}`
+        ssItemsCardText.appendChild(span)
+      ssItemsCard.appendChild(ssItemsCardText)
+    showSearchedItems.appendChild(ssItemsCard)
+  })
+}
+
+function displayCurrentMovie(item, isShow){
+  let imageText = document.querySelector('.imageText')
+  let titleText = document.querySelector('.titleText')
+  let releasedText = document.querySelector('.releasedText')
+  let ratedText = document.querySelector('.ratedText')
+  let genreText = document.querySelector('.genreText')
+  let directionText = document.querySelector('.directionText')
+  let ratingText = document.querySelector('.ratingText')
+  let plotText = document.querySelector('.plotText')
+  if(isShow){
+    imageText.src = `${item.Poster}`
+    titleText.textContent = `${item.Title}`
+    releasedText.textContent = `${item.Released}`
+    ratedText.textContent = `${item.Rated}`
+    genreText.textContent = `${item.Genre}`
+    directionText.textContent = `${item.Director}`
+    ratingText.textContent = `${item.Ratings[0].Value}`
+    plotText.textContent = `${item.Plot}`
+  }else{
+    imageText.src = ``
+    titleText.textContent = ``
+    releasedText.textContent = ``
+    ratedText.textContent = ``
+    genreText.textContent = ``
+    directionText.textContent = ``
+    ratingText.textContent = ``
+    plotText.textContent = ``
+  }
+
+}
+
 let readMoreButton = document.querySelectorAll('.readMore')
 readMoreButton.forEach((x,i)=>{
   x.addEventListener('click', async(e)=>{
-    console.log('click', i+1)
-    let response = await fetch('/getInfo', {method: 'GET', headers: {'Content-type': 'application/json; charset=UTF-8'}});
+    let response = await fetch('/getInfo', {method: 'POST', body: JSON.stringify({
+      imdbID: e.currentTarget.classList[e.currentTarget.classList.length - 1],
+    }), headers: {'Content-type': 'application/json; charset=UTF-8'}});
     let dataInJson = await response.json()
+    displayCurrentMovie(dataInJson.itemInfo, true)
     menuUi.style.display = 'grid'
+    mainBody.style.overflow = 'hidden'
   })
 })
 
+let showSearchedItems = document.querySelector('.showSearchedItems')
+let searchInput = document.querySelector('.searchInput')
+
+searchInput.addEventListener('input', async (e)=>{
+  let searchUi = document.querySelector('.showSearchedItems')
+  if(e.currentTarget.value){
+    searchUi.style.display = 'flex'
+    let response = await fetch('/search', {method: 'POST', body: JSON.stringify({
+      input: e.currentTarget.value
+    }), headers: {'Content-type': 'application/json; charset=UTF-8'}});
+    let dataInJson = await response.json()
+    console.log(dataInJson.msg)
+    loadSearchedItem(dataInJson.msg)
+  }else{
+    let showSearchedItems = document.querySelector('.showSearchedItems')
+    showSearchedItems.innerHTML = ''
+    searchUi.style.display = 'none'
+
+  }
+})
+
+
 
 showMenuButton.addEventListener('click', ()=>{
-  if(isMenuShowing == false){
-    // menuUi.style.
-    menuUi.style.display = 'none'
-  }else{
-    menuUi.style.display = 'grid'
-  }
+  menuUi.style.display = 'none'
+  mainBody.style.overflow = 'visible'
+  displayCurrentMovie(dataInJson.itemInfo, false)
+
 })
--- server.js
+++ server.js
@@ -1,9 +1,14 @@
+var bodyParser = require('body-parser')
 const express = require('express')
 const app = express()
 
 
 app.use('/public', express.static('public'))
 app.set('view engine', 'ejs')
+app.use(express.json({extended: true}))
+app.use(express.urlencoded({extended: false}))
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 const API_KEY = 'bffafdd3'
 const GetTopRowMoviesURL = `https://www.omdbapi.com/?apikey=${API_KEY}&s=spider&type=movie`
@@ -55,11 +60,28 @@ app.get('/', async (req, res)=>{
   }
   // res.render('index.ejs', {movies: placeHolderItems})
 })
-app.get('/getInfo', (req, res)=>{
-  console.log('getting into server')
-  res.json({msg:'hello'})
+app.post('/getInfo', async (req, res)=>{
+  let reqBodyImdbId = req.body.imdbID
+  let getSpecificItem = await fetch(`http://www.omdbapi.com/?apikey=${API_KEY}&i=${reqBodyImdbId}`)
+  let getSpecificItemInJson = await getSpecificItem.json()
+  res.json({itemInfo: getSpecificItemInJson})
 })
+app.post('/search',async (req, res)=>{
+  let reqString = req.body.input
+  let getSpecificItemMovie = await fetch(`http://www.omdbapi.com/?apikey=${API_KEY}&s=${reqString}&type=movie`)
+  let getSpecificItemInJsonMovie = await getSpecificItemMovie.json()
+  let getSpecificItemShow = await fetch(`http://www.omdbapi.com/?apikey=${API_KEY}&s=${reqString}&type=series`)
+  let getSpecificItemInJsonShow = await getSpecificItemShow.json()
+  let show = getSpecificItemInJsonShow.Search || []
+  let movie = getSpecificItemInJsonMovie.Search || []
+  let showsAndMovies = [...show, ...movie]
 
+  // console.log(showsAndMovies)
+  res.json({msg: showsAndMovies})
+  // res.render('index.ejs', {
+  //   searched: 
+  // })
+})
 
 
 
